f169b1320c61dc046aacd917b21f488776be501d
Keishi <33033769+anthzhou@users.noreply.github.com>
2023-03-20 19:15:27 +0100

    dropout + eraly stopping

--- network_config.py
+++ network_config.py
@@ -1,4 +1,4 @@
-se_init_config = {
+init_config = {
 
     'conv1': {'config': {'in_channels': 3, 'out_channels': 64, 'kernel_size': 3, 'padding': 1, 'stride': 1},
               'inbound_nodes': ['input']},
@@ -20,4 +20,34 @@ se_init_config = {
     'relu3': {'config': '', 'inbound_nodes': ['bn3']},
 
     'fc1': {'config': {'input_size': 256*8*8, 'output_size': 10}, 'inbound_nodes': ['relu3']},
+}
+
+dropout_config = {
+
+    'conv1': {'config': {'in_channels': 3, 'out_channels': 64, 'kernel_size': 3, 'padding': 1, 'stride': 1},
+              'inbound_nodes': ['input']},
+    'bn1': {'config': {'input_size': 64}, 'inbound_nodes': ['conv1']},
+    'relu1': {'config': '', 'inbound_nodes': ['bn1']},
+
+    'dropout1': {'config': {'dropout_rate': 0.2}, 'inbound_nodes': ['relu1']},
+
+    'max_pooling1': {'config': {'kernel_size': 2, 'stride': 2}, 'inbound_nodes': ['dropout1']},
+
+    'conv2': {'config': {'in_channels': 64, 'out_channels': 128, 'kernel_size': 3, 'padding': 1, 'stride': 1},
+              'inbound_nodes': ['max_pooling1']},
+    'bn2': {'config': {'input_size': 128}, 'inbound_nodes': ['conv2']},
+    'relu2': {'config': '', 'inbound_nodes': ['bn2']},
+
+    'dropout2': {'config': {'dropout_rate': 0.2}, 'inbound_nodes': ['relu2']},
+
+    'max_pooling2': {'config': {'kernel_size': 2, 'stride': 2}, 'inbound_nodes': ['dropout2']},
+
+    'conv3': {'config': {'in_channels': 128, 'out_channels': 256, 'kernel_size': 3, 'padding': 1, 'stride': 1},
+              'inbound_nodes': ['max_pooling2']},
+    'bn3': {'config': {'input_size': 256}, 'inbound_nodes': ['conv3']},
+    'relu3': {'config': '', 'inbound_nodes': ['bn3']},
+
+    'dropout3': {'config': {'dropout_rate': 0.2}, 'inbound_nodes': ['relu3']},
+
+    'fc1': {'config': {'input_size': 256*8*8, 'output_size': 10}, 'inbound_nodes': ['dropout3']},
 }
